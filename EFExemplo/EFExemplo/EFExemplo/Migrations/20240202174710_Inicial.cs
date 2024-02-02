2b822db56f3d6f7d7e1259059a7a828225b04c7b
Rafaele Ribeiro <rafaeleribeiro212@gmail.com>
2024-02-02 17:05:31 -0300

    Projeto Parcial

---
+++ EFExemplo/EFExemplo/EFExemplo/Migrations/20240202174710_Inicial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFExemplo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    CLienteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataNascimento = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.CLienteId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
---
+++ EFExemplo/EFExemplo/EFExemplo/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace EFExemplo.Models
+{
+    public class Cliente
+    {
+        public Guid CLienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public DateOnly DataNascimento { get; set; }
+    }
+}
